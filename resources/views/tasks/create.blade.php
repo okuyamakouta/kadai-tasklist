31446262289965cc554c2dfa544f9c8ac9741a8c
kouta okuyama <kouta1130okuyama@gmail.com>
2021-01-05 08:52:23 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+
 <h1>新規作成ページ</h1>
 <div class ="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
                 @csrf
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
