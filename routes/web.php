c2b049e624f86ba29d65440657452275bb9609e6
kouta okuyama <kouta1130okuyama@gmail.com>
2021-01-21 09:39:11 +0000

    1/21 18:40 tasklist

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::resource('tasks', 'TasksController');
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
